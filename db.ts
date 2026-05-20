a02473d972b664cdb9f0043208cdd9a041965ae0
carlos-sweb <c4rl0sill3sc4@protonmail.com>
2026-05-20 10:35:51 -0400

    add site table to sqlite database

--- db.ts
+++ db.ts
@@ -3,6 +3,7 @@ import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core"
 import { sql } from "drizzle-orm";
 import { items } from "./src/db/schema/items.ts"
 import { mdp } from "./src/db/schema/mdp.ts"
+import { site } from "./src/db/schema/site.ts"
 import { Database } from "bun:sqlite"
 
 const sqlite = new Database(process.env.DATABASE_URL);
@@ -24,7 +25,21 @@ const result = await db.select({
 .where(sql`${items.category_id} = 5`)
 */
 
-const result = await db.select().from(mdp);
 
-console.log(result);
-console.log(result.length);
+var main = {};
+
+const result = await db.select().from(site);
+
+for( const index in result){
+    const row = result[index];
+    if( !main.hasOwnProperty(row.page)  ){ main[row.page] = {} }
+    if( !main[row.page].hasOwnProperty(row.section)  ){ main[row.page][row.section] = {} }
+    if( !main.hasOwnProperty(main[row.page][row.section][row.property])  ){ main[row.page][row.section][row.property] = row.value }
+}
+
+
+console.log(main)
+//console.log(result)
+
+
+
